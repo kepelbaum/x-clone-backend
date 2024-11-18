790e9267d4ae227aab78fff399b762375ca2fea4
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-11-17 19:48:23 -0500

    fix: add missing dependencies

--- src/main/java/com/xc/x_clone_backend/gif/GifController.java
+++ src/main/java/com/xc/x_clone_backend/gif/GifController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RestController
 @RequestMapping("/api/gifs")
