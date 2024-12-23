304ef7e0960286e16c63424e66ce08831b21be70
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-12-22 19:29:33 -0500

    fix: attempting to fix date conversion

--- src/main/java/com/xc/x_clone_backend/post/PostController.java
+++ src/main/java/com/xc/x_clone_backend/post/PostController.java
@@ -70,7 +70,7 @@ public class PostController {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         post.setUsername(username);
         // post.setDate(new Date());
-        post.setDate(Date.from(offsetDateTime.toInstant()));
+        post.setDate(Date.from(java.time.OffsetDateTime.now(java.time.ZoneOffset.UTC).toInstant()));
 
         Post createdPost = postService.addPost(post);
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
